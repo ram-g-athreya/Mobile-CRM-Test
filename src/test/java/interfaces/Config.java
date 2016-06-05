5b0ae400aff61450133fddeb40b62d32bb539b1b
ramathreya <ram@carandbike.com>
2016-06-05 09:51:41 +0530

    GitIgnore & BaseURL Change

--- src/test/java/interfaces/Config.java
+++ src/test/java/interfaces/Config.java
@@ -4,6 +4,6 @@ package interfaces;
  * Created by ramathreya on 04/06/16.
  */
 public interface Config {
-    public static final String BASE_URL = "http://localhost:3000";
+    public static final String BASE_URL = "http://ec2-52-23-186-41.compute-1.amazonaws.com:3000";
     public static final boolean DEBUG = false;
 }
