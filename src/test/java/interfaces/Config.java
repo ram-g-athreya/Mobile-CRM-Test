3ec76a0a6b3417f0c4db57cfb0825ea084834966
ramathreya <ram@carandbike.com>
2016-07-15 09:21:21 +0530

    Test

--- src/test/java/interfaces/Config.java
+++ src/test/java/interfaces/Config.java
@@ -4,6 +4,6 @@ package interfaces;
  * Created by ramathreya on 04/06/16.
  */
 public interface Config {
-    public static final String BASE_URL = "http://ec2-107-22-141-68.compute-1.amazonaws.com:3000";
+    public static final String BASE_URL = "http://ec2-52-91-169-238.compute-1.amazonaws.com:3000";
     public static final boolean DEBUG = false;
 }
